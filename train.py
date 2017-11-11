85569d1978c5177fcc01230fc7a57f0834055459
Lingcao Huang <huanglingcao@gmail.com>
2017-11-11 15:49:14 +0800

    fix error: Negative dimension size caused by subtracting 2 from 1 for 'max_pooling2d_1/MaxPool' (op: 'MaxPool') with input shapes

--- train.py
+++ train.py
@@ -71,15 +71,15 @@ def get_unet():
 
     conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(pool1)
     conv2 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2),dim_ordering="th")(conv2)
 
     conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(pool2)
     conv3 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2),dim_ordering="th")(conv3)
 
     conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(pool3)
     conv4 = Convolution2D(256, 3, 3, activation='relu', border_mode='same')(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2),dim_ordering="th")(conv4)
 
     conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same')(pool4)
     conv5 = Convolution2D(512, 3, 3, activation='relu', border_mode='same')(conv5)
